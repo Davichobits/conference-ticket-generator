0c3a87ad2f9751874760fea4f7bd2442a357ec8e
Davichobits <davidruizdeveloper@gmail.com>
2025-10-21 15:29:40 -0500

    Update TicketGenerated component for improved responsiveness and styling on larger screens

--- src/components/ticket-generated.tsx
+++ src/components/ticket-generated.tsx
@@ -4,24 +4,24 @@ interface Props {
 
 export const TicketGenerated = ({ styles='' }: Props) => {
   return (
-    <div className={`${styles} bg-[url(images/pattern-ticket.svg)] bg-no-repeat bg-contain w-[342px] h-[160px] m-auto p-4 relative md:w-[600px] md:h-[280px]`}>
+    <div className={`${styles} bg-[url(images/pattern-ticket.svg)] bg-no-repeat bg-contain w-[342px] h-[160px] m-auto p-4 relative md:w-[600px] md:h-[280px] md:py-[30px] md:px-6`}>
 
       <div className='flex flex-col justify-between h-full'>
         <div>
-          <img src="/images/logo-full.svg" alt="logo full" />
-          <p className='text-sm text-Neutral-500 ml-12 mb-8'>Jan 31, 2025 / Austing, TX</p>
+          <img className='md:w-[240px]' src="/images/logo-full.svg" alt="logo full" />
+          <p className='text-sm text-Neutral-500 ml-12 mb-8 md:text-lg'>Jan 31, 2025 / Austing, TX</p>
         </div>
         <div className='flex gap-3'>
           <img
             alt="imageavatar"
             src="/images/image-avatar.jpg"
-            className='size-[45px] rounded-lg'
+            className='size-[45px] rounded-lg md:size-[80px]'
           />
           <div>
-            <p className='text-xl font-medium'>Jontan Kristof</p>
+            <p className='text-xl font-medium md:text-3xl'>Jontan Kristof</p>
             <p className='flex'>
               <img src="/images/icon-github.svg" alt="" />
-              <span>@jonatankristof0101</span>
+              <span className='md:text-xl'>@jonatankristof0101</span>
             </p>
           </div>
         </div>
