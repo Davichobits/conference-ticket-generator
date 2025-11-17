4b5d0c2bb2426416ddf1b6219eed8fa429fb6bd0
Davichobits <davidruizdeveloper@gmail.com>
2025-11-17 17:00:08 -0500

    image buttons rendered conditionally

---
+++ src/components/ticket-form-page/form/image-buttons.tsx
@@ -0,0 +1,8 @@
+export const ImageButtons = () => {
+  return (
+    <div className='flex justify-center gap-4'>
+      <button className='bg-Neutral-300/10 px-2 rounded-lg cursor-pointer hover:bg-Neutral-300/15'>Remove Image</button>
+      <button className='bg-Neutral-300/10 px-2 rounded-lg cursor-pointer hover:bg-Neutral-300/15'>Change Image</button>
+    </div>
+  )
+}
--- src/components/ticket-form-page/form/upload-input/upload-input.tsx
+++ src/components/ticket-form-page/form/upload-input/upload-input.tsx
@@ -1,4 +1,5 @@
 import { InfoIcon } from '../../../info-icon';
+import { ImageButtons } from '../image-buttons';
 
 interface Props {
   url?: string;
@@ -15,10 +16,12 @@ export const UploadInput = ({
   ...rest
 }: Props) => {
 
+  const isImage = url !== '';
+
   return (
     <div className='mb-4'>
       <p className='mb-4 text-xl'>Upload Avatar</p>
-      <label className='border-3 border-dashed px-4 py-3 block w-full border-Neutral-0 bg-Neutral-700/50 hover:bg-Neutral-700 rounded-xl cursor-pointer mb-1'>
+      <label className={`border-3 border-dashed px-4 py-3 block w-full border-Neutral-0 bg-Neutral-700/50  rounded-xl mb-1 ${isImage ? '' : 'hover:bg-Neutral-700 cursor-pointer '}`}>
         <div className='bg-Neutral-700 size-[50px] grid place-content-center rounded-xl border-2 border-Neutral-500 mb-2 mx-auto overflow-hidden'>
           <img src={url ? url : '/images/icon-upload.svg'} alt="icon upload" />
         </div>
@@ -28,7 +31,9 @@ export const UploadInput = ({
           className='hidden'
           type="file"
         />
-        <span className='text-center block text-gray-500 text-sm'>Drag and drop or click to upload</span>
+        {
+          isImage ? (<ImageButtons/>) : (<span className='text-center block text-gray-500 text-sm'>Drag and drop or click to upload</span>)
+        }
       </label>
         {
           isError ? 
